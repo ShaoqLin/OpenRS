ae71e5fcb8f1c9baebdc68631ce7de4adca48673
ShaoqLin <852884179@qq.com>
2023-11-19 22:15:28 +0800

    add new configs and new run.sh

--- opendet2/tools/run.sh
+++ opendet2/tools/run.sh
@@ -5,7 +5,7 @@ python tools/train_net.py \
 --resume \
 --num-gpus 1 \
 --config-file \
-/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_1e-3_bs16_opendet_dota-dior.yaml
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_1e-1_bs8_opendet_dota-dior37.yaml
 
 sleep 60
 
@@ -15,7 +15,7 @@ python tools/train_net.py \
 --resume \
 --num-gpus 1 \
 --config-file \
-/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_2e-3_bs16_opendet_dota-dior.yaml
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_1e-1_bs16_opendet_dota-dior37.yaml
 
 sleep 60
 
@@ -25,7 +25,7 @@ python tools/train_net.py \
 --resume \
 --num-gpus 1 \
 --config-file \
-/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_3e-3_bs16_opendet_dota-dior.yaml
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_1e-2_bs8_opendet_dota-dior37.yaml
 
 sleep 60
 
@@ -35,7 +35,7 @@ python tools/train_net.py \
 --resume \
 --num-gpus 1 \
 --config-file \
-/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_4e-3_bs16_opendet_dota-dior.yaml
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_1e-2_bs16_opendet_dota-dior37.yaml
 
 sleep 60
 
@@ -45,7 +45,77 @@ python tools/train_net.py \
 --resume \
 --num-gpus 1 \
 --config-file \
-/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_5e-3_bs16_opendet_dota-dior.yaml
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_1e-3_bs8_opendet_dota-dior37.yaml
+
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_1e-3_bs16_opendet_dota-dior37.yaml
+
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_2e-2_bs8_opendet_dota-dior37.yaml
+
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_2e-2_bs16_opendet_dota-dior37.yaml
+
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_2e-3_bs8_opendet_dota-dior37.yaml
+
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_2e-3_bs16_opendet_dota-dior37.yaml
+
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_3e-3_bs8_opendet_dota-dior37.yaml
+
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior_3_7/faster_rcnn_R_50_FPN_3x_3e-3_bs16_opendet_dota-dior37.yaml
 
 sleep 60
 
@@ -58,4 +128,4 @@ python /mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/too
 # --resume \
 # --num-gpus 1 \
 # --config-file \
-# /mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_5e-2_bs8_opendet_dota-dior.yaml
+# /mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_5e-2_bs8_opendet_dota-dior37.yaml
