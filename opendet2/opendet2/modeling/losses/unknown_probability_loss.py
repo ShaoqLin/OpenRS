39f7386c2d2bf2bafe6a7ce62a88d155d8e7e91e
ShaoqLin <852884179@qq.com>
2023-11-22 22:51:47 +0800

    add prob score from PROB

--- opendet2/opendet2/modeling/losses/unknown_probability_loss.py
+++ opendet2/opendet2/modeling/losses/unknown_probability_loss.py
@@ -91,3 +91,20 @@ class UPLoss(nn.Module):
             (1-gt_scores[num_fg:]).pow(self.alpha)
 
         return self._soft_cross_entropy(mask_scores, targets.detach())
+
+
+# added ProbObjectness from PROB
+class ProbObjectnessHead(nn.Module):
+    def __init__(self, hidden_dim):
+        super().__init__()
+        self.flatten = nn.Flatten(0,1)
+        self.objectness_bn = nn.BatchNorm1d(hidden_dim, affine=False)
+
+    def freeze_prob_model(self):
+        self.objectness_bn.eval()
+        
+    def forward(self, x):
+        out=self.flatten(x)
+        out=self.objectness_bn(out).unflatten(0, x.shape[:2])
+        return out.norm(dim=-1)**2
+    
