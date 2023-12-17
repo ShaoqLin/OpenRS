2fe49af12a5ec9ddbeefa59e5640e65e9d072172
ShaoqLin <852884179@qq.com>
2023-12-17 15:00:31 +0800

    modified configs

---
+++ opendet2/tools/run.sh
@@ -0,0 +1,41 @@
+# too big learning rate to Nan
+sleep 23400
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_1e-3_bs16_baseline_dota-dior_10_15_20.yaml
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_1e-3_bs16_opendet_dota-dior_10_15_20.yaml
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_1e-3_bs16_opendet_gmm_dota-dior_10_15_20.yaml
+sleep 60
+
+CUDA_VISIBLE_DEVICES=2 \
+DETECTRON2_DATASETS=/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/data/datasets \
+python tools/train_net.py \
+--resume \
+--num-gpus 1 \
+--config-file \
+/mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/opendet2/configs/dota_dior/faster_rcnn_R_50_FPN_3x_1e-3_bs16_opendet_siren_dota-dior_10_15_20.yaml
+sleep 60
+
+python /mnt/bee9bc2f-b897-4648-b8c4-909715332cb4/linshaoqing/projects/OpenRS/tools/alldone.py
+RSgis104
